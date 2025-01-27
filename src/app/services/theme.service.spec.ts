281eecb96abe1046b198a1fdb01179e33cff35d9
Tim <timgomperts@gmail.com>
2025-01-27 17:16:16 +0100

    Added theme service and first test

---
+++ src/app/services/theme.service.spec.ts
@@ -0,0 +1,20 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ThemeService } from './theme.service';
+
+describe('ThemeService', () => {
+  let service: ThemeService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ThemeService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should initialize with light theme by default', () => {
+    expect(service.isDarkMode()).toBeFalse();
+  });
+});
