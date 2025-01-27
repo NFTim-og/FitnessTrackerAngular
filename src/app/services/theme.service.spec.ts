bf09275c0f55e533ac7e3a1eebcdfb4a64d49522
Tim <timgomperts@gmail.com>
2025-01-27 17:25:17 +0100

    Added all the tests for the theme service (with errors)

--- src/app/services/theme.service.spec.ts
+++ src/app/services/theme.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ThemeService } from './theme.service';
 
 describe('ThemeService', () => {
@@ -8,6 +7,8 @@ describe('ThemeService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(ThemeService);
+    // Clear localStorage before each test
+    localStorage.clear();
   });
 
   it('should be created', () => {
@@ -16,5 +17,30 @@ describe('ThemeService', () => {
 
   it('should initialize with light theme by default', () => {
     expect(service.isDarkMode()).toBeFalse();
+  });
+
+  it('should toggle theme', () => {
+    expect(service.isDarkMode()).toBeFalse();
+    service.toggleTheme();
+    expect(service.isDarkMode()).toBeTrue();
+    service.toggleTheme();
+    expect(service.isDarkMode()).toBeFalse();
+  });
+
+  it('should persist theme preference', () => {
+    service.toggleTheme(); // Enable dark mode
+    expect(localStorage.getItem('darkMode')).toBe('true');
+    
+    // Create new instance to test persistence
+    const newService = TestBed.inject(ThemeService);
+    expect(newService.isDarkMode()).toBeTrue();
+  });
+
+  it('should notify subscribers when theme changes', (done) => {
+    service.darkMode$.subscribe(isDark => {
+      expect(isDark).toBeTrue();
+      done();
+    });
+    service.toggleTheme();
   });
 });
