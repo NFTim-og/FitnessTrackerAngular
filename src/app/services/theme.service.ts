7ffa53f86b70dba8983c979fdaf832d821e13a37
Tim <timgomperts@gmail.com>
2025-01-27 17:28:59 +0100

    Added the theme service code and updated the global_styles.css to apply the logic of the component

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,10 +1,38 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
+  private darkModeSubject = new BehaviorSubject<boolean>(false);
+  darkMode$ = this.darkModeSubject.asObservable();
+
+  constructor() {
+    // Load saved preference from localStorage
+    const savedTheme = localStorage.getItem('darkMode');
+    if (savedTheme) {
+      this.darkModeSubject.next(savedTheme === 'true');
+      this.updateTheme(savedTheme === 'true');
+    }
+  }
+
   isDarkMode(): boolean {
-    return false;
+    return this.darkModeSubject.value;
+  }
+
+  toggleTheme(): void {
+    const newValue = !this.darkModeSubject.value;
+    this.darkModeSubject.next(newValue);
+    localStorage.setItem('darkMode', newValue.toString());
+    this.updateTheme(newValue);
+  }
+
+  private updateTheme(isDark: boolean): void {
+    if (isDark) {
+      document.documentElement.classList.add('dark-mode');
+    } else {
+      document.documentElement.classList.remove('dark-mode');
+    }
   }
 }
